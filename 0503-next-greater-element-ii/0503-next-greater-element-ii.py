8ae836dc2c13d09cfa85f45fb8d890cad3c8df05
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-02-09 21:01:23 +0530

    Time: 4601 ms (7.69%), Space: 13.6 MB (78.67%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -7,11 +7,9 @@ class Solution(object):
         res = [-1]*len(nums)
         for i in range(len(nums)):
             li = nums[i+1:] + nums[:i]
-            flag = True
             for j in li:
                 if j > nums[i]:
                     res[i] = j
-                    flag = False
                     break
             
         return res
