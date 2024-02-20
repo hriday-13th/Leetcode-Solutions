a3607b3ded2c669cf680ed21bac29e377e3b2fc6
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-02-20 13:38:44 +0530

    Time: 126 ms (67.90%), Space: 18.1 MB (88.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero_pointer = 0
+        non_zero = 0
         
-        while zero_pointer != len(nums) and len(nums) > 1:
-            if nums[zero_pointer] != 0:
-                zero_pointer += 1
-            else:
-                non_zero_pointer = zero_pointer + 1
-                while non_zero_pointer < len(nums) and nums[non_zero_pointer] == 0:
-                    non_zero_pointer += 1
-                if non_zero_pointer < len(nums):
-                    nums[zero_pointer], nums[non_zero_pointer] = nums[non_zero_pointer], nums[zero_pointer]
-                zero_pointer += 1
+        for i in nums:
+            if i != 0:
+                nums[non_zero] = i
+                non_zero += 1
+                
+        while non_zero < len(nums):
+            nums[non_zero] = 0
+            non_zero += 1
+        
+        
+#         zero_pointer = 0
+        
+#         while zero_pointer != len(nums) and len(nums) > 1:
+#             if nums[zero_pointer] != 0:
+#                 zero_pointer += 1
+#             else:
+#                 non_zero_pointer = zero_pointer + 1
+#                 while non_zero_pointer < len(nums) and nums[non_zero_pointer] == 0:
+#                     non_zero_pointer += 1
+#                 if non_zero_pointer < len(nums):
+#                     nums[zero_pointer], nums[non_zero_pointer] = nums[non_zero_pointer], nums[zero_pointer]
+#                 zero_pointer += 1
