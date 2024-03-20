1bda035307710a45b6ccf8bc2e898da0e38a4d07
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-03-20 19:36:59 +0530

    Time: 41 ms (52.17%), Space: 16.6 MB (63.88%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,23 +1,33 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {}
+        res = [False] * (len(s) + 1)
+        res[0] = True
         
-        def cal(t, words):
-            if t in memo:
-                return memo[t]
-            if t == "":
-                return True
+        for i in range(len(s) + 1):
+            if res[i] == True:
+                for word in wordDict:
+                    if s[i:].startswith(word):
+                        res[i + len(word)] = True
+                        
+        return res[-1]
+#         memo = {}
+        
+#         def cal(t, words):
+#             if t in memo:
+#                 return memo[t]
+#             if t == "":
+#                 return True
             
-            for w in words:
-                if t.startswith(w):
-                    suff = t[len(w):]
-                    if cal(suff, words):
-                        memo[t] = True
-                        return memo[t]
-                else:
-                    continue
+#             for w in words:
+#                 if t.startswith(w):
+#                     suff = t[len(w):]
+#                     if cal(suff, words):
+#                         memo[t] = True
+#                         return memo[t]
+#                 else:
+#                     continue
                     
-            memo[t] = False
-            return memo[t]
+#             memo[t] = False
+#             return memo[t]
         
-        return cal(s, wordDict)
+#         return cal(s, wordDict)
