3fb6b6724bb337d53e34564f08dbf65d10ca0cd7
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-04-01 08:26:25 +0530

    Time: 14 ms (52.85%), Space: 12.2 MB (9.58%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -4,5 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        spl = s.split()
-        return len(spl[-1])
+        count = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i].isalpha():
+                count += 1 
+            if s[i] == " " and count != 0:
+                break
+        return count
+        # spl = s.split()
+        # return len(spl[-1])
