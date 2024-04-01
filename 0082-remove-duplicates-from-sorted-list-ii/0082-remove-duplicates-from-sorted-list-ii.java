228746c684ee0304fc636f4b96e37d02ac16f264
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-04-01 12:47:55 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (68.53%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,33 +10,50 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null){
-            return head;
-        }
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ListNode curr = head;
-        
-        while (curr != null){
-            int val = curr.val;
-            map.put(val, map.getOrDefault(val, 0) + 1);
-            curr = curr.next;
-        }
-        
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
+        ListNode dummy = new ListNode(0, head);
         ListNode prev = dummy;
-        curr = head;
         
-        while (curr != null){
-            int val = curr.val;
-            if (map.containsKey(val) && map.get(val) > 1) {
-                prev.next = curr.next;
-            } else {
-                prev = curr;
+        while (head != null){
+            if (head.next != null && head.val == head.next.val){
+                while (head.next != null && head.val == head.next.val){
+                    head = head.next;
+                }
+                prev.next = head.next;
             }
-            curr = curr.next;
+            else{
+                prev = prev.next;
+            }
+            head = head.next;
         }
         return dummy.next;
     }
 }
+//         if (head == null || head.next == null){
+//             return head;
+//         }
+        
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         ListNode curr = head;
+        
+//         while (curr != null){
+//             int val = curr.val;
+//             map.put(val, map.getOrDefault(val, 0) + 1);
+//             curr = curr.next;
+//         }
+        
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         ListNode prev = dummy;
+//         curr = head;
+        
+//         while (curr != null){
+//             int val = curr.val;
+//             if (map.containsKey(val) && map.get(val) > 1) {
+//                 prev.next = curr.next;
+//             } else {
+//                 prev = curr;
+//             }
+//             curr = curr.next;
+//         }
+//         return dummy.next;
+//     }
