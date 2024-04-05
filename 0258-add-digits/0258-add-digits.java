d494316473e5bdffe564c55758862d8dfecf629a
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-04-05 20:54:34 +0530

    Time: 1 ms (47.93%), Space: 40.9 MB (30.96%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        if (num < 10){
+            return num;
+        }
+        
+        while (num >= 10){
+            int temp = 0;
+            while (num > 0){
+                temp += num % 10;
+                num /= 10;
+            }
+            num = temp;
+        }
+        return num;
+    }
+}
