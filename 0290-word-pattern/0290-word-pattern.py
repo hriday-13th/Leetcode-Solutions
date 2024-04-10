b64f5bbde6dbef87b5ca8051e0c66605f3a84400
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-04-11 02:30:23 +0530

    Time: 30 ms (84.32%), Space: 16.4 MB (99.79%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -2,13 +2,11 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         d = {}
         ls = s.split()
-        seen = set()
         
         if len(pattern) != len(ls):
             return False
         
         for i in range(len(pattern)):
-            # print(i, d, pattern[i], ls[i], sep="|")
             if pattern[i] not in d.keys():
                 if ls[i] not in d.values():
                     d[pattern[i]] = ls[i]
