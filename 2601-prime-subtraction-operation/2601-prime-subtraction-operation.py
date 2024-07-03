871c77abb6428a8eb2898594c74942f6b4544ccf
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-07-04 02:24:23 +0530

    Time: 278 ms (19.42%), Space: 16.6 MB (81.55%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        prime = [True] * 1001
+        prime[0] = prime[1] = False
+        
+        for x in range(2, 1001):
+            if prime[x]:
+                for j in range(x*x, 1001, x):
+                    prime[j] = False
+                    
+        prev = 0
+        
+        for x in nums:
+            if prev >= x:
+                return False
+            for p in range(x - 1, -1, -1):
+                if prime[p] and x - p > prev:
+                    break
+                    
+            prev = x - p
+            
+        return True
