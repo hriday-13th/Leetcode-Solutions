7723c28ec7af6359d6d5add873bcc15a1d878597
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-07-11 00:41:43 +0530

    Time: 140 ms (13.55%), Space: 19.3 MB (46.72%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,17 +1,45 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        if n == 1:
-            return 1
+        candies = n
+        i = 1
         
-        candies = [1] * n
+        while i < n:
+            if ratings[i] == ratings[i-1]:
+                print(i)
+                i += 1
+                continue
+                
+            peak = 0
+            while i < n and ratings[i] > ratings[i-1]:
+                print(i)
+                peak += 1
+                candies += peak
+                i += 1
+                if candies == n: return candies
+                
+            dip = 0
+            while i < n and ratings[i] < ratings[i-1]:
+                print(i)
+                dip += 1
+                candies += dip
+                i += 1
+                
+            candies -= min(peak, dip)
+            
+        return candies
+#         n = len(ratings)
+#         if n == 1:
+#             return 1
         
-        for i in range(1, n):
-            if ratings[i] > ratings[i-1]:
-                candies[i] = candies[i-1] + 1
+#         candies = [1] * n
+        
+#         for i in range(1, n):
+#             if ratings[i] > ratings[i-1]:
+#                 candies[i] = candies[i-1] + 1
                 
-        for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1]:
-                candies[i] = max(candies[i], candies[i+1] + 1)
+#         for i in range(n-2, -1, -1):
+#             if ratings[i] > ratings[i+1]:
+#                 candies[i] = max(candies[i], candies[i+1] + 1)
                 
-        return sum(candies)
+#         return sum(candies)
