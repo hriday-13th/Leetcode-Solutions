1c1b23aea7fc080c0910a9ebc0985d40bf44fe62
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-07-14 00:35:46 +0530

    Time: 35 ms (68.89%), Space: 16.6 MB (21.35%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         queue = deque()
+#         if root is None:
+#             return []
+        
+#         if root.left is None and root.right is None:
+#             return [root.val]
+        
+#         result = []
+#         queue.append(root)
+#         while queue:
+#             child_queue = deque()
+#             prev = -1
+#             while queue:
+#                 curr = queue.popleft()
+
+#                 if curr.left is not None:
+#                     child_queue.append(curr.left)
+
+#                 if curr.right is not None:
+#                     child_queue.append(curr.right)
+                
+#                 prev = curr
+            
+#             result.append(prev.val)
+#             queue = child_queue
+        
+#         return result
+        q = deque()
+        if root is None:
+            return []
+        if root.left is None and root.right is None:
+            return [root.val]
+        
+        res = []
+        q.append(root)
+        
+        while q:
+            child_q = deque()
+            prev = -1
+            
+            while q:
+                curr = q.popleft()
+                if curr.left is not None:
+                    child_q.append(curr.left)
+                if curr.right is not None:
+                    child_q.append(curr.right)
+                prev = curr
+                
+            res.append(prev.val)
+            q = child_q
+            
+        return res
