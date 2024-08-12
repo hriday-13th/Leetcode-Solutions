a9739e129ca5a9b114eee909a44b80db28606a37
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-08-12 23:34:32 +0530

    Time: 49 ms (32.87%), Space: 14.8 MB (96.30%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,11 +1,25 @@
-class Solution:
-    def countBattleships(self, board: List[List[str]]) -> int:
-        count = 0
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        rows, cols = len(board), len(board[0])
         
-        for i, row in enumerate(board):
-            for j, cell in enumerate(row):
-                if cell == "X":
-                    if (i == 0 or board[i-1][j] == ".") and (j == 0 or board[i][j-1] == "."):
-                        count += 1
-                        
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] == ".":
+                return
+            board[r][c] = "."
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+            
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "X":
+                    dfs(r, c)
+                    count += 1
+                    
         return count
