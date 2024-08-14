afe414da7ea15db2f142a603ad9e47fb688912c2
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-08-14 10:59:04 +0530

    Time: 20 ms (44.59%), Space: 11.9 MB (11.15%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,16 +1,20 @@
-class Solution:
-    def isAlienSorted(self, words: List[str], order: str) -> bool:
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
         if len(words) == 1:
             return True
         
-        order_map = defaultdict()
-        
-        for ind, ele in enumerate(order):
-            order_map[ele] = ind
+        order_map = {}
+        for i, val in enumerate(order):
+            order_map[val] = i
             
         for i in range(len(words) - 1):
             for j in range(len(words[i])):
-                if j >= len(words[i + 1]):
+                if j >= len(words[i+1]):
                     return False
                 
                 if words[i][j] != words[i + 1][j]:
