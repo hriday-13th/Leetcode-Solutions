0fc31d9ea29e3e0d2219a266a5f2844bde1876e1
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-08-29 02:50:23 +0530

    Time: 2253 ms (83.33%), Space: 73.5 MB (50.00%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,7 +1,12 @@
-class Solution:
-    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        res = 0
+class Solution(object):
+    def countSubIslands(self, grid1, grid2):
+        """
+        :type grid1: List[List[int]]
+        :type grid2: List[List[int]]
+        :rtype: int
+        """
         rows, cols = len(grid2), len(grid2[0])
+        res = 0
         
         def dfs(i, j):
             if i < 0 or i >= rows or j < 0 or j >= cols or grid2[i][j] != 1:
@@ -17,7 +22,7 @@ class Solution:
             is_sub &= dfs(i, j - 1)
             
             return is_sub
-            
+        
         for i in range(rows):
             for j in range(cols):
                 if grid2[i][j] == 1 and dfs(i, j):
