6bdb8714ecf21a5430d6469060a59e7bb7586d34
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-09-15 19:33:47 +0530

    Time: 43 ms (16.51%), Space: 16.8 MB (24.45%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [nums]
-        
-        for i in range(len(nums)):
-            res.extend(list(itertools.combinations(nums, i)))
-            
+        def backtrack(start, path):
+            res.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+                
+        res = []
+        backtrack(0, [])
         return res
+#         res = [nums]
+        
+#         for i in range(len(nums)):
+#             res.extend(list(itertools.combinations(nums, i)))
+            
+#         return res
