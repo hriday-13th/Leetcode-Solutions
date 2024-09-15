e1f012f0275798bf75e9abcb2fa57a6a68a8e410
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-09-15 14:00:04 +0530

    Time: 2225 ms (84.12%), Space: 45.8 MB (47.16%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,45 +1,24 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-#         rows, cols = len(grid), len(grid[0])
-#         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        rows, cols = len(grid), len(grid[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         
-#         q = deque([(0, 0, 0)])
-#         visited = set([(0, 0)])
+        q = deque([(0, 0, 0)])
+        visited = set([(0, 0)])
         
-#         while q:
-#             i, j, count = q.popleft()
+        while q:
+            i, j, count = q.popleft()
             
-#             if i == rows - 1 and j == cols - 1:
-#                 return count
-                
-#             for x, y in directions:
-#                 ni, nj = i + x, j + y
-#                 if 0 <= ni < rows and 0 <= nj < cols and (ni, nj) not in visited:
-#                     visited.add((ni, nj))
-#                     if grid[ni][nj] == 1:
-#                         q.append((ni, nj, count + 1))
-#                     else:
-#                         q.append((ni, nj, count))
-                        
-#         return -1
-        queue = deque()
-        dir = [[1,0],[0,1],[-1,0],[0,-1]]
-        visited = set()
-        n = len(grid)
-        m = len(grid[0])
-        queue.append((0, 0, 0))
-        visited.add((0, 0))
-        while queue:
-            i,j,count = queue.popleft()
-            if i == n - 1 and j == m - 1:
+            if i == rows - 1 and j == cols - 1:
                 return count
-            for x,y in dir:
-                dx = x+i
-                dy = y+j
-                if dx>=0 and dx<n and dy>=0 and dy<m and (dx, dy) not in visited:
-                    visited.add((dx, dy))
-                    if grid[dx][dy] == 1:
-                        queue.append((dx, dy, count + 1))
+                
+            for x, y in directions:
+                ni, nj = i + x, j + y
+                if 0 <= ni < rows and 0 <= nj < cols and (ni, nj) not in visited:
+                    visited.add((ni, nj))
+                    if grid[ni][nj] == 1:
+                        q.append((ni, nj, count + 1))
                     else:
-                        queue.appendleft((dx, dy, count))
+                        q.appendleft((ni, nj, count))
+                        
         return -1
