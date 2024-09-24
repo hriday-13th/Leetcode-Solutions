21014e07d4c56da2aa9d9174862da88b0a7a193f
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-09-25 02:26:58 +0530

    Time: 25 ms (43.75%), Space: 11.9 MB (14.06%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,10 +1,14 @@
-class Solution:
-    def pancakeSort(self, arr: List[int]) -> List[int]:
+class Solution(object):
+    def pancakeSort(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
         res = []
         
-        for x in range(len(arr), 1, -1):
-            i = arr.index(x)
-            res.extend([i + 1, x])
-            arr = arr[i + 1:][::-1] + arr[:i]
+        for val in range(len(arr), 1, -1):
+            i = arr.index(val)
+            res.extend([i+1, val])
+            arr = arr[i+1:][::-1] + arr[:i]
         
         return res
