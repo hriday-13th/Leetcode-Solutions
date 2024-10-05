771994191aaf1d1525b963c94c030e39110d7630
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-10-05 19:19:18 +0530

    Time: 42 ms (98.27%), Space: 16.8 MB (22.10%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        m, n = len(s1), len(s2)
+        
+        s1_count, s2_count = [0] * 26, [0] * 26
+        
+        if m > n:
+            return False
+        
+        for i in range(m):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+            
+        if s1_count == s2_count:
+            return True
+        
+        for i in range(m, n):
+            s2_count[ord(s2[i]) - ord('a')] += 1
+            s2_count[ord(s2[i-m]) - ord('a')] -= 1
+            
+            if s1_count == s2_count:
+                return True
+            
+        return False
