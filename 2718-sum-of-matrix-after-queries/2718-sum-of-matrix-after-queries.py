a8b543ead0e0efb3e3008f43f94ac3b478885b4e
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-10-06 23:57:31 +0530

    Time: 1480 ms (85.00%), Space: 37.1 MB (40.00%) - LeetHub

--- 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.py
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.py
@@ -1,42 +1,14 @@
-class Solution:
-    def matrixSumQueries(self, n: int, queries: List[List[int]]) -> int:
-        if n == 1:
-            return queries[-1][-1]
-        
-        rows = [(0, 0)] * n
-        cols = [(0, 0)] * n
-        
-        for i in range(len(queries)):
-            if queries[i][0] == 0:
-                rows[queries[i][1]] = (i+1, queries[i][2])
-            elif queries[i][0] == 1:
-                cols[queries[i][1]] = (i+1, queries[i][2])
-                
-        rows.sort(key=lambda x: x[0], reverse=True)
-        cols.sort(key=lambda x: x[0], reverse=True)
-        
+class Solution(object):
+    def matrixSumQueries(self, n, queries):
+        seenrows, seencols = set(), set()
         res = 0
-        i, j = 0, 0
-        col_val, row_val = n, n
         
-        while i < n and j < n:
-            if rows[i][0] > cols[j][0]:
-                res += rows[i][1] * row_val
-                col_val -= 1
-                i += 1
-            else:
-                res += cols[j][1] * col_val
-                row_val -= 1
-                j += 1
+        for q, i, val in queries[::-1]:
+            if q == 0 and i not in seenrows:
+                seenrows.add(i)
+                res += val * (n - len(seencols))
+            elif q == 1 and i not in seencols:
+                seencols.add(i)
+                res += val * (n - len(seenrows))
                 
-        while i < n:
-            res += rows[i][1] * row_val
-            col_val -= 1
-            i += 1
-            
-        while j < n:
-            res += cols[j][1] * col_val
-            row_val -= 1
-            j += 1
-            
         return res
