411e20853a08411f2683bf37e816c76b3aad0a6c
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-10-10 02:03:48 +0530

    Time: 452 ms (58.50%), Space: 29 MB (32.00%) - LeetHub

---
+++ 2860-happy-students/2860-happy-students.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countWays(self, nums: List[int]) -> int:
+        nums.sort()
+        count = 0
+        
+        if nums[0] > 0:
+            count += 1
+        if nums[-1] < len(nums):
+            count += 1
+            
+        for i in range(len(nums) - 1):
+            s = i + 1
+            if nums[i] < s and nums[i + 1] > s:
+                count += 1
+            
+        return count
