e450153eee71534fc11f6bd11615a997ed7e62f3
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-10-19 00:27:29 +0530

    Time: 219 ms (100.00%), Space: 130.5 MB (5.30%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,4 +1,14 @@
-from itertools import combinations
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(combinations(list(range(1, n + 1)), k))
+class Solution(object):
+    def combine(self, n, k):
+        def backtrack(start, path, k):
+            if k == 0:
+                res.append(path[:])
+                return
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1, path, k - 1)
+                path.pop()
+                
+        res = []
+        backtrack(1, [], k)
+        return res
