50ea124b77cc5d92eb312fff409e812f51456a52
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-10-31 15:51:45 +0530

    Time: 19 ms (86.79%), Space: 16.9 MB (13.89%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,22 +1,18 @@
-class Solution(object):
-    def minimumMountainRemovals(self, nums):
-        n = len(nums)
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        def foo(arr):
+            dp = [float('inf')] * (len(arr) + 1)
+            l = [0] * len(arr)
+            for i, ele in enumerate(arr):
+                l[i] = bisect_left(dp, ele) + 1
+                dp[l[i] - 1] = ele
+            return l
         
-        inc = [1] * n
-        for i in range(n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    inc[i] = max(inc[i], inc[j] + 1)
+        l1, l2 = foo(nums), foo(nums[::-1])[::-1]
+        res, n = 0, len(nums)
         
-        dec = [1] * n
-        for i in range(n-1, -1, -1):
-            for j in range(i+1, n):
-                if nums[j] < nums[i]:
-                    dec[i] = max(dec[i], dec[j] + 1)
-                    
-        res = 0
         for i in range(n):
-            if inc[i] > 1 and dec[i] > 1:
-                res = max(res, inc[i] + dec[i] - 1)
+            if l1[i] > 1 and l2[i] > 1:
+                res = max(res, l1[i] + l2[i] - 1)
                 
         return n - res
