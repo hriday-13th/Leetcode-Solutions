06c847820f25e91baf781fc882f68908bcad0a56
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-11-01 12:58:07 +0530

    Time: 10469 ms (10.00%), Space: 13.6 MB (95.51%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,9 +1,23 @@
-class Solution:
-    def makeFancyString(self, s: str) -> str:
-        res = []
+class Solution(object):
+    def makeFancyString(self, s):
+        res = ""
+        prev1, prev2 = None, None
         
-        for c in s:
-            if len(res) < 2 or not (res[-1] == res[-2] == c):
-                res.append(c)
+        for i in s:
+            if prev1 == None:
+                prev1 = i
+                res += i
+                continue
+            elif prev2 == None:
+                prev2 = prev1
+                prev1 = i
+                res += i
+                continue
+            elif i == prev1 and i == prev2:
+                continue
+            else:
+                res += i
+                prev2 = prev1
+                prev1 = i
                 
-        return "".join(res)
+        return res
