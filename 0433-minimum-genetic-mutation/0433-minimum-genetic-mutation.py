23a6aee4383edb8645b36e81e640940b903a5640
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-11-03 00:36:01 +0530

    Time: 0 ms (100.00%), Space: 16.8 MB (8.18%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def minMutation(self, startGene, endGene, bank):
-        def check(a, b):
+class Solution:
+    def minMutation(self, s: str, e: str, bank: List[str]) -> int:
+        def checker(a, b):
             return sum(1 for i in range(8) if a[i] != b[i]) == 1
         
-        q = deque([startGene])
-        visited = set(startGene)
-        
+        q = deque([s])
+        visited = set(s)
         res = 0
         
         while q:
             for _ in range(len(q)):
                 curr = q.popleft()
-                if curr == endGene:
+                if curr == e:
                     return res
                 for nbr in bank:
-                    if check(nbr, curr) and nbr not in visited:
+                    if checker(nbr, curr) and nbr not in visited:
                         visited.add(nbr)
                         q.append(nbr)
             res += 1
-            
+        
         return -1
