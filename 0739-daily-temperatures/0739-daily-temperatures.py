a15f73c188dc3c4ffcc61e60f0b1678e4e9b67f1
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-12-10 09:20:09 +0530

    Time: 131 ms (28.59%), Space: 34.2 MB (7.35%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,12 +4,9 @@ class Solution:
         stack = deque()
         
         for i, temp in enumerate(temperatures):
-            if not stack or stack[-1][0] > temp:
-                stack.append([temp, i])
-            else:
-                while stack and temp > stack[-1][0]:
-                    pop_temp, pop_i = stack.pop()
-                    res[pop_i] = i - pop_i
-                stack.append([temp, i])
+            while stack and temp > stack[-1][0]:
+                pop_temp, pop_i = stack.pop()
+                res[pop_i] = i - pop_i
+            stack.append([temp, i])
             
         return res
