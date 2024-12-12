0af00c9c0b2dd4d30bf3e00122b14c379061d509
Hriday Pradhan <hridaypradhan201003@gmail.com>
2024-12-12 12:07:57 +0530

    Time: 449 ms (93.99%), Space: 12.2 MB (94.67%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        prev = [0] * (len(text2) + 1)
+
+        for i in range(1, len(text1) + 1):
+            curr = [0] * (len(text2) + 1)
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    curr[j] = prev[j - 1] + 1
+                else:
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr
+
+        return prev[len(text2)]
